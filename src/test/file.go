3ce46de33b4d1c8d17a1606a6c95430eae9bf16b
tianyunchong <854024109@qq.co>
2016-06-05 22:03:16 +0800

    提交下修改

---
+++ src/test/file.go
@@ -0,0 +1,15 @@
+// ini config read
+package main
+
+import (
+    "flag"
+    "fmt"
+)
+
+var (
+    configFile = flag.String("configfile", "config.ini", "General configuration file")
+)
+vat TOPIC = make(map[string]string)
+func main() {
+    
+}
